08c269d9aa055e5c48fadd8791094e0c5a4977cc
coco860731 <sao92407@gmail.com>
2017-12-11 18:05:54 +0800

    LED control

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -74,7 +74,7 @@ else:
 while 1:
 	
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-	SwitchStatus = GPIO.input(24)
+	SwitchStatus =1-GPIO.input(24)
 	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
 	if humidity is not None and temperature is not None:
    		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
@@ -86,9 +86,3 @@ while 1:
 	else:
    		print('Failed to get reading. Try again!')
     		sys.exit(1)
-
-
-	if( SwitchStatus == 0):
-		print('button pressed')
-	else:
-		print('button released')
